88ada45eb4eca7ce0b0d270775835875e0404195
Julian Neri <julian@Julians-MacBook-Pro.local>
2020-11-12 17:35:11 -0800

    Update to shell.

--- run.sh
+++ run.sh
@@ -4,11 +4,7 @@ python3 -m venv env
 source env/bin/activate
 pip3 install --upgrade pip
 
-pip3 install torch
-pip3 install torchvision
-pip3 install numpy
-pip3 install matplotlib
-pip3 install scipy
+pip3 install -r requirements.txt
 
 echo "Running Python script"
 python evaluate.py
