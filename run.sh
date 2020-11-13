d91f23f91cc040269d1cdb9b84f3edd14cd3823f
Julian Neri <julian@Julians-MacBook-Pro.local>
2020-11-12 16:25:16 -0800

    Initial commit.

--- run.sh
+++ run.sh
@@ -2,11 +2,11 @@
 
 python3 -m venv env
 source env/bin/activate
-pip install --upgrade pip
+pip3 install --upgrade pip
 
 pip3 install torch
 pip3 install numpy
 pip3 install matplotlib
 
 echo "Running Python script"
-python train.py
+python evaluate.py
