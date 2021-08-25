bd1f569cca9c4f1a1e975d1c0fc174187ebdc959
Julian Neri <43224513+jundsp@users.noreply.github.com>
2021-08-25 17:04:09 -0400

    Created dataloader.py

---
+++ src/dataloader.py
@@ -0,0 +1,14 @@
+import torch
+import torch.utils.data
+from torchvision import datasets, transforms
+
+
+def get_data_loaders(directory,batch_size=64):
+    train_set = datasets.MNIST(directory, train=True, download=True, transform=transforms.ToTensor())
+    test_set = datasets.MNIST(directory, train=False, download=True, transform=transforms.ToTensor())
+
+    train_loader = torch.utils.data.DataLoader(train_set,batch_size=batch_size, shuffle=True, **kwargs)
+    test_loader = torch.utils.data.DataLoader(test_set,batch_size=batch_size, shuffle=True, **kwargs)
+
+    return train_loader, test_loader
+
